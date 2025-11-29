a59f74d5b011e7e8cdce59440ca7166585ca4714
sebastian camacho <secamc93@gmail.com>
2025-11-29 14:59:40 -0500

    feat - refactor probability

---
+++ back/central/cmd/main.go
@@ -0,0 +1,37 @@
+// main.go
+
+// @title						Restaurant Reservation API
+// @version					1.0
+// @description				Servicio REST para la gestión de reservas multi-restaurante.
+// @termsOfService				https://ejemplo.com/terminos
+//
+// @contact.name				Equipo de Backend
+// @contact.email				backend@example.com
+//
+// @license.name				MIT
+// @license.url				https://opensource.org/licenses/MIT
+//
+// @host						localhost:3050
+// @BasePath					/api/v1
+//
+// @securityDefinitions.apikey	BearerAuth
+// @in							header
+// @name						Authorization
+// @description				Token de business JWT con el prefijo **Bearer** (para todos los endpoints)
+//
+// @securityDefinitions.apikey	BusinessTokenAuth
+// @in							header
+// @name						Authorization
+// @description				Token principal JWT con el prefijo **Bearer** (solo para /auth/business-token)
+package main
+
+import (
+	"context"
+
+	"central/cmd/internal/server"
+)
+
+func main() {
+	_ = server.Init(context.Background())
+	select {}
+}
