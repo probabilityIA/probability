7910ab730c2ff214e2a5e3014ff39aa4806043ef
SanCam04 <sancam04@hotmail.com>
2026-06-02 11:49:29 -0500

    fix(navbar): use flaticon URL directly for notification icon instead of local file

--- front/central/src/shared/ui/sidebar.tsx
+++ front/central/src/shared/ui/sidebar.tsx
@@ -533,7 +533,7 @@ export function Sidebar({ user }: SidebarProps) {
                         style={{ backgroundColor: 'var(--color-tertiary)' }}
                       />
                     )}
-                    <img src="/notification-icon.png" alt="Notificaciones" className="w-5 h-5 flex-shrink-0 object-contain" />
+                    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384162.png" alt="Notificaciones" className="w-5 h-5 flex-shrink-0 object-contain" />
                     {primaryExpanded && (
                       <span className="text-sm font-medium transition-opacity duration-300">Notificaciones</span>
                     )}
